ab5620c7b9fd311d8dff488ff1ca1731b35ee856
Andrew Fecheyr <andrew@bedesign.be>
2017-11-05 22:08:13 +0100

    Run phoenix in production

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,12 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pricey, PriceyWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "${HOST}", port: {:system, "PORT"}],
+  secret_key_base: "${SECRET_KEY_BASE}",
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  version: Mix.Project.config[:version],
+  server: true,
+  root: "."
 
 # Do not print debug messages in production
 config :logger, level: :info
